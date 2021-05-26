ed77f3f5d750b2e4e3c12c6848f36e70f1d89dca
Hui Yu <ityuhui@gmail.com>
2021-05-26 22:45:49 +0800

    ssl debug

--- k8s_wsc.c
+++ k8s_wsc.c
@@ -4,13 +4,13 @@
 
 void attach_data_callback(void **p_data_received, long *p_data_received_len)
 {
-    printf("%s: Received %ld bytes: %s", __func__,*p_data_received_len, (char *)*p_data_received);
+    printf("%s: Received %ld bytes: %s", __func__, *p_data_received_len, (char *)*p_data_received);
 }
 
 void exec(wsclient_t *wsc, const char *cmd)
 {
     wsclient_run(wsc, cmd);
-    printf("Received %ld bytes: %s\n", wsc->data_received_len, (char *)wsc->data_received);
+    printf("%s: Received %ld bytes: %s\n", __func__, wsc->data_received_len, (char *)wsc->data_received);
 }
 
 void attach(wsclient_t *wsc)
@@ -23,16 +23,16 @@ void attach(wsclient_t *wsc)
 int main(int argc, char *argv[])
 {
     const char *ws_server_address = "192.168.22.117";
-    const char *ws_path = "/api/v1/namespaces/default/pods/test-pod-8/exec?command=ls&container=my-container&stdin=true&stdout=true&tty=true";
+    const char *ws_path = "/api/v1/namespaces/default/pods/test-pod-8/exec?command=bash&container=my-container&stdin=true&stdout=true&tty=true";
     int ws_port = 6443;
 
     sslConfig_t *sslconfig = (sslConfig_t *) calloc(sizeof(sslConfig_t), 1);
-    sslconfig->CACertFile = strdup("");
-    sslconfig->clientCertFile = strdup("");
-    sslconfig->clientKeyFile = strdup("");
+    sslconfig->CACertFile = strdup("/root/k8s_cert/kubeconfig-7ucPpD");
+    sslconfig->clientCertFile = strdup("/root/k8s_cert/kubeconfig-81noCw");
+    sslconfig->clientKeyFile = strdup("/root/k8s_cert/kubeconfig-Ilh604");
     sslconfig->insecureSkipTlsVerify = 0;
 
-    wsclient_t *wsc = wsclient_create(ws_server_address, ws_path, ws_port, NULL);
+    wsclient_t *wsc = wsclient_create(ws_server_address, ws_path, ws_port, sslconfig);
     if (!wsc) {
         fprintf(stderr, "Cannot create a websocket client.\n");
         return -1;
